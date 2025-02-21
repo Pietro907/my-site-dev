cc1e86a08e5e286b98d53d5f0fc096e4da0645d6
Akiba777 <pietrop.puri@gmail.com>
2025-02-21 13:00:14 +0100

    init pre-page-site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../component/macro-component/header/header.component";
+import { MainComponent } from "../component/macro-component/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [/* RouterOutlet ,*/ HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
